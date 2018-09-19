36cbd640f195cf5115c7b0090c974e80c63d50ba
Cyrex <szwathub@gmail.com>
2018-09-19 18:05:11 +0800

    Initial commit

---
+++ Examples/SPhotoBookKitExample/SViewController.m
@@ -0,0 +1,26 @@
+//
+//  Copyright © 2018 ZhiweiSun. All rights reserved.
+//
+//  File name: SViewController.m
+//  Author:    ZhiweiSun @Cyrex
+//  E-mail:    szwathub@gmail.com
+//
+//  Description:
+//
+//  History:
+//      09/19/2018: Created by Cyrex on 09/19/2018
+//
+
+#import "SViewController.h"
+
+@interface SViewController ()
+
+@end
+
+@implementation SViewController
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
---
+++ SPhotoBookKit/SPhotoBookKit.h
@@ -0,0 +1,27 @@
+//
+//  Copyright © 2018 ZhiweiSun. All rights reserved.
+//
+//  File name: SPhotoBookKit.h
+//  Author:    ZhiweiSun @Cyrex
+//  E-mail:    szwathub@gmail.com
+//
+//  Description:
+//
+//  History:
+//      09/19/2018: Created by Cyrex on 09/19/2018
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SPhotoBookKit.
+FOUNDATION_EXPORT double SPhotoBookKitVersionNumber;
+
+//! Project version string for SPhotoBookKit.
+FOUNDATION_EXPORT const unsigned char SPhotoBookKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPhotoBookKit/PublicHeader.h>
+#if __has_include(<SPhotoBookKit/SPhotoBookKit.h>)
+// #import <SPhotoBookKit/PublicHeader.h>
+#else
+// #import "PublicHeader.h"
+#endif
